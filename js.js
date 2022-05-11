ee3538ab52eacb005176a364dbaed54128ffdabc
ct2k <voicesfromthedarkside@protonmail.com>
2022-05-11 12:32:07 -0700

    Basic conditional statement added. Game working with 1 player.

--- js.js
+++ js.js
@@ -5,6 +5,12 @@
 const btnDraw = document.querySelector('.drawcard');
 const btnNewGame = document.querySelector('.newgame');
 const drawCard = document.querySelector('.randomcard');
+const showResults = document.querySelector('.results');
+const player0Score = document.querySelector('.score--0');
+
+// Scores
+
+let currentScore = 0;
 
 // Random card pick
 
@@ -12,7 +18,16 @@ btnDraw.addEventListener('click', function () {
   const randomCard = Math.floor(Math.random() * 21) + 1;
   drawCard.classList.remove('hidden');
   drawCard.textContent = randomCard;
+  currentScore += randomCard;
+  player0Score.textContent = currentScore;
   console.log(randomCard);
+  if (currentScore === 21) {
+    showResults.classList.remove('hidden');
+    showResults.textContent = 'You rolled a 21 and won the game!';
+  } else if (currentScore > 21) {
+    showResults.classList.remove('hidden');
+    showResults.textContent = 'You lose the game!';
+  }
 });
 
 btnNewGame.addEventListener('click', function () {
