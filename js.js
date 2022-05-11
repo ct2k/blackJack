9577506f35d1cc9d5224ef3e92b55b953d8120c7
ct2k <voicesfromthedarkside@protonmail.com>
2022-05-11 13:41:16 -0700

    Fixed randomCard draw which was 21 instead of 11. Added a function that does a separate card draw for the dealer.

--- js.js
+++ js.js
@@ -7,6 +7,21 @@ const btnNewGame = document.querySelector('.newgame');
 const drawCard = document.querySelector('.randomcard');
 const showResults = document.querySelector('.results');
 const player0Score = document.querySelector('.score--0');
+const player1Score = document.querySelector('.score--1');
+
+// Functions
+
+const dealerRoll = [Math.floor(Math.random() * 11) + 1, Math.floor(Math.random() * 11) + 1];
+let dealerScore = 0;
+
+for (let i = 0; i < dealerRoll.length; i++) {
+  const dealerDraw = function () {
+    if (dealerRoll[0]) player1Score.textContent = dealerScore; // Only show the first roll by the dealer
+    return (dealerScore += dealerRoll[i]);
+  };
+  dealerDraw();
+}
+console.log('Dealer draws a ' + dealerScore);
 
 // Scores
 
@@ -15,12 +30,12 @@ let currentScore = 0;
 // Random card pick
 
 btnDraw.addEventListener('click', function () {
-  const randomCard = Math.floor(Math.random() * 21) + 1;
+  const randomCard = Math.floor(Math.random() * 11) + 1;
   drawCard.classList.remove('hidden');
   drawCard.textContent = randomCard;
   currentScore += randomCard;
   player0Score.textContent = currentScore;
-  console.log(randomCard);
+  console.log('Player draws a ' + randomCard);
   if (currentScore === 21) {
     showResults.classList.remove('hidden');
     showResults.textContent = 'You rolled a 21 and won the game!';
