d4d4085f615c180ae41271bcf745edaa6000cbbe
ct2k <voicesfromthedarkside@protonmail.com>
2022-05-11 11:59:55 -0700

    Layout updated. Old, unused JS code removed. Was not suppose to be in the first real commit.

--- js.js
+++ js.js
@@ -1,39 +1,5 @@
-// Blackjack
+'use strict';
+
+// Selecting elements
 
 // Random card pick
-
-const randomCard = function () {
-  return Math.floor(Math.random() * 11) + 1;
-};
-// console.log(randomCard());
-
-let playerCards;
-score = 0;
-
-// Draw card
-
-const startBtn = document.querySelector('.startgame');
-const showScore = document.querySelector('.score');
-
-// const startGame = function () {
-//   if (playerCards === 21) {
-//     showScore.textContent = score + ' You win Blackjack!';
-//   } else if (playerCards > 21) {
-//     showScore.textContent = score + ' You lose the game!';
-//   } else {
-//     showScore.textContent = score + ' Draw a new card?';
-//   }
-// };
-
-// startBtn.addEventListener('click', startGame);
-// console.log(startGame());
-
-const drawBtn = document.querySelector('.drawcard');
-
-drawBtn.addEventListener('click', drawCard);
-
-function drawCard() {
-  randomCard();
-}
-
-console.log(drawCard());
