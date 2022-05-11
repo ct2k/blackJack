e7791e32805e570574b36dad0eab330df71cfd4f
ct2k <voicesfromthedarkside@protonmail.com>
2022-05-11 12:09:45 -0700

    Basic draw card functionality working.

--- js.js
+++ js.js
@@ -2,4 +2,19 @@
 
 // Selecting elements
 
+const btnDraw = document.querySelector('.drawcard');
+const btnNewGame = document.querySelector('.newgame');
+const drawCard = document.querySelector('.randomcard');
+
 // Random card pick
+
+btnDraw.addEventListener('click', function () {
+  const randomCard = Math.floor(Math.random() * 21) + 1;
+  drawCard.classList.remove('hidden');
+  drawCard.textContent = randomCard;
+  console.log(randomCard);
+});
+
+btnNewGame.addEventListener('click', function () {
+  console.log(btnNewGame.textContent);
+});
